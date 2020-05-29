df0ef1a4596a1103323f56334e781729eb523a0c
supraj24 <61577897+supraj24@users.noreply.github.com>
2020-05-29 15:42:19 -0700

    Update agent.sv

--- code/agent.sv
+++ code/agent.sv
@@ -1,19 +1,21 @@
 //Generator and Driver communication is done by agent class
+import agent_pkg::*;
+
 class agent;
-mailbox generator2agent_rd_t, generator2agent_wr_t,             //generator to agent for read and write operation
-		agent2driver_rd_t, agent2driver_wr_t;             		//agent to driver for read and write operation
-mailbox agent2sboard_rd_t, agent2sboard_wr_t;             		//agent to scoreboard for read and write operation
+mailbox generator2agent_rd_h, generator2agent_wr_h,             //generator to agent for read and write operation
+		agent2driver_rd_h, agent2driver_wr_h;             		//agent to driver for read and write operation
+mailbox agent2sboard_rd_h, agent2sboard_wr_h;             		//agent to scoreboard for read and write operation
 pkt_read  pkt_rd;
 pkt_write pkt_wr;
 
 //Function for creation of new objects
 function new(mailbox gen_agt_mbRd, gen_agt_mbWr, agt_dri_mbRd, agt_dri_mbWr, agt_scb_mbRd, agt_scb_mbWr);
-	this.generator2agent_rd_t	= gen_agt_mbRd;
-	this.generator2agent_wr_t 	= gen_agt_mbWr;
-	this.agent2driver_rd_t 		= agt_dri_mbRd;
-	this.agent2driver_wr_t 		= agt_dri_mbWr;
-	this.agent2sboard_rd_t 		= agt_scb_mbRd;
-	this.agent2sboard_wr_t 		= agt_scb_mbWr;
+	this.generator2agent_rd_h	= gen_agt_mbRd;
+	this.generator2agent_wr_h 	= gen_agt_mbWr;
+	this.agent2driver_rd_h 		= agt_dri_mbRd;
+	this.agent2driver_wr_h 		= agt_dri_mbWr;
+	this.agent2sboard_rd_h 		= agt_scb_mbRd;
+	this.agent2sboard_wr_h 		= agt_scb_mbWr;
 endfunction
 
 // Task for agent communication between generator and driver
@@ -24,12 +26,12 @@ task run();
 		forever                    //***always ??
 		begin
 			//Write channel
-            if(generator2agent_wr_t.num() != 0)
+            if(generator2agent_wr_h.num() != 0)
 			begin
-				generator2agent_wr_t.get(pkt_wr);               		//agent gets transaction from generator
+				generator2agent_wr_h.get(pkt_wr);               		//agent gets transaction from generator
 				$display( "[%0t] Got Write packet at agent", $time);    //**debug then remove
-				agent2driver_wr_t.put(pkt_wr);                			//agent sends transaction to driver
-				agent2sboard_wr_t.put(pkt_wr);                			//agent sends transaction to scoreboard
+				agent2driver_wr_h.put(pkt_wr);                			//agent sends transaction to driver
+				agent2sboard_wr_h.put(pkt_wr);                			//agent sends transaction to scoreboard
 			end
 			else
 				break;    					
@@ -39,12 +41,12 @@ task run();
 		forever                    //***always ??
 		begin
 			//Read channel
-			if(generator2agent_rd_t.num() != 0)
+			if(generator2agent_rd_h.num() != 0)
 			begin
-				generator2agent_rd_t.get(pkt_rd);            		 //agent gets transaction from generator
+				generator2agent_rd_h.get(pkt_rd);            		 //agent gets transaction from generator
 				$display("[%0t] Got Read packet at agent", $time);   //debug then remove
-				agent2driver_rd_t.put(pkt_rd);             			//agent sends transaction to driver
-				agent2sboard_rd_t.put(pkt_rd);             			//agent sends transaction to scoreboard
+				agent2driver_rd_h.put(pkt_rd);             			//agent sends transaction to driver
+				agent2sboard_rd_h.put(pkt_rd);             			//agent sends transaction to scoreboard
 			end
 			else
 				break; 
